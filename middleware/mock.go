d6853477f8de88251e460aaff8507b0c3fd3b237
Doug Weber <dweber@reisenderlabs.net>
2021-12-20 16:54:02 +0100

    add a mock

--- middleware/mock.go
+++ middleware/mock.go
@@ -1,6 +1,9 @@
 package middleware
 
 import (
+	"bufio"
+	"bytes"
+	"fmt"
 	"go-api"
 	"net/http"
 )
@@ -19,4 +22,20 @@ func NewMock(mock api.Dofn) api.Middleware {
 		}
 
 	}
+}
+
+// MockResponse mocks a response object and can be used with NewMock.
+func MockResponse(mock func(req *http.Request) (statusCode int, respBody string)) api.Dofn {
+	// handler that returns the resonse directly and doesn't pass on to the next
+	return func(req *http.Request) (*http.Response, error) {
+		statusCode, respBody := mock(req)
+		buf := bytes.NewBufferString(fmt.Sprintf("HTTP/1.1 %d\n\n%s", statusCode, respBody))
+		return http.ReadResponse(bufio.NewReader(buf), req)
+	}
+}
+
+// NewMockResponse is a convenience func that combines NewMock and MockResponse
+func NewMockResponse(mock func(req *http.Request) (statusCode int, respBody string)) api.Middleware {
+	// return the middleware func
+	return NewMock(MockResponse(mock))
 }
